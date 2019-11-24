589a6965346fc4a7d277833c54108b8ae86cbc57
Praffulsingh9 <prafful.singh017@gmail.com>
2019-11-24 21:30:33 +0530

    Clarifai integrated for face detection

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
 import Particles from "react-particles-js";
+import Clarifai from "clarifai";
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Rank from "./components/Rank/Rank";
+import FaceImageRecognition from "./components/FaceImageRecognition/FaceImageRecognition";
 import "./App.css";
 const particlesOptions = {
   particles: {
@@ -17,7 +19,44 @@ const particlesOptions = {
   }
 };
 
+const app = new Clarifai.App({
+  apiKey: "a368369741bc4ff7b1dd8b8e4cb3ad80"
+});
+
 class App extends Component {
+  state = {
+    input: "",
+    imageUrl: "",
+    box: {},
+    route: "signin",
+    isSignedIn: false,
+    user: {
+      id: "",
+      name: "",
+      email: "",
+      entries: 0,
+      joined: ""
+    }
+  };
+  onInputChange = event => {
+    this.setState({ input: event.target.value });
+  };
+
+  onButtonSubmit = () => {
+    this.setState({ imageUrl: this.state.input });
+    app.models
+      .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+      .then(
+        response => {
+          if (response) {
+            console.log(response);
+          }
+        }
+        //this.displayFaceBox(this.calculateFaceLocation(response))
+      )
+      .catch(err => console.log(err));
+  };
+
   render() {
     return (
       <div className="App">
@@ -25,7 +64,11 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        <FaceImageRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
       </div>
     );
   }
