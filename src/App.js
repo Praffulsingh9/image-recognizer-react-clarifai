bcd33ea57e3638ffcaf250b36afe1c2b8624ce4a
Praffulsingh9 <prafful.singh017@gmail.com>
2019-11-24 21:45:37 +0530

    Box added for detected face

--- src/App.js
+++ src/App.js
@@ -38,24 +38,54 @@ class App extends Component {
       joined: ""
     }
   };
+
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    this.setState({box: box});
+  }
+
   onInputChange = event => {
     this.setState({ input: event.target.value });
   };
 
   onButtonSubmit = () => {
-    this.setState({ imageUrl: this.state.input });
+    this.setState({imageUrl: this.state.input});
     app.models
-      .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-      .then(
-        response => {
-          if (response) {
-            console.log(response);
-          }
+      .predict(
+        Clarifai.FACE_DETECT_MODEL,
+        this.state.input)
+      .then(response => {
+        if (response) {
+          fetch('http://localhost:3000/image', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              id: this.state.user.id
+            })
+          })
+            .then(response => response.json())
+            .then(count => {
+              this.setState(Object.assign(this.state.user, { entries: count}))
+            })
+
         }
-        //this.displayFaceBox(this.calculateFaceLocation(response))
-      )
+        this.displayFaceBox(this.calculateFaceLocation(response))
+      })
       .catch(err => console.log(err));
-  };
+  }
 
   render() {
     return (
